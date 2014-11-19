68c599201fe3399fd06f55537f44fbd3ab7aef15
Anthony Castelli <neueanthony@me.com>
2014-11-18 21:39:51 -0800

    logging: Remove this useless stuff

--- Toon HQ WatchKit Extension/InterfaceController.swift
+++ Toon HQ WatchKit Extension/InterfaceController.swift
@@ -23,7 +23,6 @@ class InterfaceController: WKInterfaceController {
         super.init(context: context)
         
         // Configure interface objects here.
-        NSLog("%@ init", self)
         
         // Define the timer as a constant as we won't be changing this
         // 15 seconds might be too fast or too slow... idk
@@ -34,12 +33,10 @@ class InterfaceController: WKInterfaceController {
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
-        NSLog("%@ will activate", self)
     }
 
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
-        NSLog("%@ did deactivate", self)
         super.didDeactivate()
     }
     
