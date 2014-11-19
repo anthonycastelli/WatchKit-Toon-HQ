a120ef9c7875683b6e92e05d2c3551695112a9e9
Anthony Castelli <neueanthony@me.com>
2014-11-18 17:04:39 -0800

    invasion: Prep everything

--- Toon HQ WatchKit Extension/InterfaceController.swift
+++ Toon HQ WatchKit Extension/InterfaceController.swift
@@ -15,12 +15,20 @@ class InterfaceController: WKInterfaceController {
     // IB Outlets
     @IBOutlet var invasionTable: WKInterfaceTable!
     
+    // Regular variables
+    var invasions: Array<Dictionary<String, AnyObject>> = []
+    
     override init(context: AnyObject?) {
         // Initialize variables here.
         super.init(context: context)
         
         // Configure interface objects here.
         NSLog("%@ init", self)
+        
+        // Define the timer as a constant as we won't be changing this
+        // 15 seconds might be too fast or too slow... idk
+        let timer = NSTimer.scheduledTimerWithTimeInterval(15, target: self, selector: "fetchInvasions", userInfo: nil, repeats: true)
+        timer.fire()
     }
 
     override func willActivate() {
@@ -33,6 +41,12 @@ class InterfaceController: WKInterfaceController {
         // This method is called when watch view controller is no longer visible
         NSLog("%@ did deactivate", self)
         super.didDeactivate()
+    }
+    
+    // MARK: TableView Data
+    
+    func fetchInvasion() {
+        
     }
 
 }
